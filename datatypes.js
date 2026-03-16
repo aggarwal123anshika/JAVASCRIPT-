2e57c9e14e1c3d243df1bc29ffdf829b8d779a00
aggarwal123anshika <anshikaagarwal9966@gmail.com>
2026-03-16 22:40:57 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,45 @@
+// let a = 10;
+// a="anshu";
+// a=true;
+// let b;
+// let sum = null;
+
+// int a = 23;
+// int b = 24;
+// int sum;
+// sum = a+b;
+
+// THIS WAY IS MORE CORRECT THAN UPPER CODE DUE TO SUM = 0 IT CONSUMES GARBAGE VALUE
+
+// int a = 23;
+// int b = 24;
+// int sum = 0;
+// sum = a+b;
+// console.log('the value of sum is '+ sum);
+// console.log("the type of sum is "+ typeof(sum));
+
+// let v1 = 12;
+// console.log(v1+1);
+
+// let v2 = "12";
+// console.log(v2+1);
+
+// let abc = 123;
+// let result = (String(abc));
+// console.log("the value of abc is",abc);
+// console.log('the value of abc is '+ typeof(result));
+// console.log("the type of abc is "+ typeof(abc));
+
+// let name = "anshika"
+// let name  = 344;
+// name = Number(name);
+// console.log("the value of name is",name);
+// console.log('the value of abc is '+ typeof(result));
+// console.log("the type of name is "+ typeof(name));
+
+
+// let name  = 0;
+// name = Boolean(name);
+// it always return true either value is positove and negative except 0(zero)
+// console.log("the value of name is",name);
+// console.log("the type of name is "+ typeof(name));
