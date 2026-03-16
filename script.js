08f6eac68232d66534cdac2f7892051cc943cdeb
aggarwal123anshika <anshikaagarwal9966@gmail.com>
2026-03-16 22:40:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// let name = "anshu";
+// let age = 28;
+// let isEligibleToVote = true;
+
+// console.log("My Name is "+name);
+// console.log("My Age is ", age);
+// console.log("Is Eligible to vote",isEligibleToVote);
+
+
+// let a = 12;
+// let b = 13;
+// let sum = a + b;
+// console.log(sum);
+// console.log(sum-1);
+
+let varName = "Anshika Aggarwal";
+// varName = "Intro to JS";
+console.log(varName);
+// varName = "Intro to JS";
+varName = 19;
+console.log(varName);
+
+const office = "ABC";
+office = "XYZ";
+console.log(office);
+
+
+
+
