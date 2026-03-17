4b995250a1bdec532856e7a9fd381ee67a708bbd
aggarwal123anshika <anshikaagarwal9966@gmail.com>
2026-03-17 19:25:08 +0530

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,53 @@
+// ELSE IF LADDER
+// const score = 50;
+
+// if(score >= 90) {
+//     console.log("A+");
+// }else if (score >= 80) {
+//     console.log("B+");
+// }else if (score >= 70) {
+//     console.log("C+");
+// }else if (score >= 60) {
+//     console.log("D");
+// }else {
+//     console.log("Need to improve");
+// }
+
+// SWITCH CONDITION 
+// const score = 80;
+// switch (score) {
+//     case 90:
+//         console.log("A+");
+//         break;
+//     case 80:
+//         console.log("B+");
+//         break;
+//     case 70:
+//         console.log("C+");
+//         break; 
+//     case 60:
+//         console.log("D+");
+//         break;
+//     default:
+//         console.log("Need to Improvement!");
+//         break;
+// }
+
+// const score = 'A';
+// switch (score) {
+//     case 'A':
+//         console.log("Excellent Work🤗");
+//         break;
+//     case 'B':
+//         console.log("Good Work🖊️");
+//         break;
+//     case 'C':
+//         console.log("Good⌛");
+//         break; 
+//     case 'D':
+//         console.log("Fair Work");
+//         break;
+//     default:
+//         console.log("Need to Improvement!");
+//         break;
+// }
