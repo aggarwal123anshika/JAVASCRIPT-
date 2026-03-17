e7ae374a196171dbdd558df6194276b8e518bd92
aggarwal123anshika <anshikaagarwal9966@gmail.com>
2026-03-17 19:06:30 +0530

    Add files via upload

---
+++ conditions.js
@@ -0,0 +1,51 @@
+// console.log("first line of the program flow");
+// const candidateAge = 15;
+
+// IF CONDITION
+// if(candidateAge >= 18) {
+//     console.log("hey you can to give a vote!");
+//     console.log("Dummy inner log");
+// }
+// console.log("hey you can vote :-)");
+
+// IF-ELSE CONDITION
+// if(candidateAge >= 18) {
+//     console.log("hey you can to give a vote!");
+//     console.log("Dummy inner log");
+// }else{
+//     console.log("hey you cannot give a vote :-)");
+// }
+
+// NESTED IF CONDITION 
+// if(candidateAge >= 18) {
+//     if(candidateAge >= 60) {
+//         console.log("senior citizens are eligible to vote online !!");
+//     }
+//     console.log("hey you can to give a vote");
+// }else {
+//     console.log("hey you cannot give a vote !");
+// }
+
+// ELSE IF LADDER
+// if(candidateAge >= 60) {
+//     console.log("senior citizens are eligible to vote online!");
+// }else if(candidateAge >= 18) {
+//     console.log("younger citizens! hey you can give a vote!");
+// }else {
+//     console.log("hey you cannot give a vote!");
+    
+// }
+
+// TERNARY OPERATOR
+// condition ? true : false ;
+const candidateAge = 20;
+(candidateAge > 18) ? console.log("eligible to give vote!"): console.log("not eligible to give a vote");
+
+
+// console.log("this is the last line of the program..");
+
+
+
+
+
+
