bff7d90f33688762a0289dc7c1afcc42295fcc09
aggarwal123anshika <anshikaagarwal9966@gmail.com>
2026-03-18 20:02:31 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,41 @@
+// FOR LOOP
+// for(let i=0 ; i<=10; i++) {
+//     console.log(i,"Anshu"); 
+// }
+
+// for(let i=0 ; i<=10; i=i+2) {
+//     console.log("Anshu",i); 
+// }
+
+// for(let i=20 ; i>0; i--) {
+//     console.log("Anshu",i); 
+// }
+
+
+// WHILE LOOP
+// console.log("While Loop!");
+// let count = 8;
+// while(count<5) {
+//     console.log(count);
+//     count++; 
+// }
+
+
+// DO WHILE LOOP
+// console.log("Do While Loop!");
+// let count2 = 8;
+// do {
+//     console.log(count2);
+//     count2++;
+// } while (count2 < 5)
+
+// CONTINUE AND BREAK
+// let count = 5;
+// while(count<10) {
+//     console.log("Hello Beautiful❤️");
+//     if(count == 6) {
+//         // break;
+//     }
+//     count++;
+// }
+
