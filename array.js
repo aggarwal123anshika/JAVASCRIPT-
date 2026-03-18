c3ddfead76637ca900065b57bb392100af593080
aggarwal123anshika <anshikaagarwal9966@gmail.com>
2026-03-18 20:03:04 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,22 @@
+// let varName = 123;
+// let arrName = [123, 456, 789];
+
+// let stud1 = "abc";
+// let stud2 = "xyz";
+// let stud3 = "pqr";
+
+let studentsArray = ["abc", "xyz", "pqr", 123, true, function dummyFunction() {console.log("dummy function")}, {name: "anshika", age: 12}];
+
+// index num          0       1       2    3    4        5                                                         6
+
+// console.log(studentsArray);
+// console.log(studentsArray[2]);
+// console.log(studentsArray[5]);
+console.log(studentsArray[6]);
+
+// java
+// String arrName = ["anshu", "aggarwal"]
+
+console.log(studentsArray.length);
+
+
