a27e9ce9999f5c7a09d389a967f564411208b095
aggarwal123anshika <anshikaagarwal9966@gmail.com>
2026-03-20 20:33:34 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,56 @@
+// console.log(document.body);
+const bodyBackground = document.body;
+// bodyBackground.style.backgroundColor = "green";
+
+// 1ST APPROACH
+// const box4 = document.getElementById('box-4');
+// console.log(box4);
+
+// 2ND APPROACH
+// const h1 = document.getElementByTagName('h1');
+// console.log(h1);
+
+// 3RD APPROACH
+// const boxes = document.getElementsByClassName('box');
+// console.log(boxes);
+
+// MODIFYING HTML ELEMENTS
+// const box1 = document.getElementById('box-1');
+// box1.innerHTML = "<b>Anshika Aggarwal</b>";
+// box1.innerText = "Anshika Aggarwal";
+
+// OR WE CAN WRITE ABOVE THREE LINES AS:-)
+// document.getElementById('box-1').innerText = "Anshika Aggarwal"
+// document.getElementsById('p').innerText = "Anshika Aggarwal"
+
+// const box1 = document.getElementById('box-1');
+// box1.style.backgroundColor = "red";
+
+// const box2 = document.getElementById('box-2');
+// box2.classList.add('circle');
+// box2.classList.remove('container');
+
+// const box3 = document.getElementById('box-3');
+// box3.classList.remove('circle');
+
+// const boxes = document.getElementsByClassName('box');
+// for(let i=0;i<boxes.height;i++) {
+//     boxes[i].classList.add('circle');
+// }
+// box3.classList.remove('box');
+
+
+// CREATING A NEW ELEMENT
+// 1st is to get the access
+// 2nd is the contents
+// 3rd is appending
+
+// const newPara = document.createElement('p');
+// newPara.innerText = "This is our new paragraph";
+
+// const container = document.getElementById('container');
+// container.appendChild(newpara);
+
+// newPara.classList.add('box');
+// newPara.classList.add('circle');
+
